a3c76ccf0ac68c9fc5ea830dfacd130b2307c990
edersantana <edersantanajunior@hotmail.com>
2015-02-14 15:55:49 -0500

    Using unittest for functional_tests

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,41 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # A person hear about a new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # The header metion to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # She is invited to enter a to-do item straight away
+
+        # She types "Biy peapock feathers" into a text box
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+
+        # There is still a text box inviting her to add another item.
+        # She enters "Use peacock feathers to make a fly"
+
+        # The page updates again, and now shows both items on her list
+
+        # The site has generated a unique URL for her --- there is
+        # some explanatory text to that effect.
+
+        # She visits that URL - her to-do list is still there
+
+        # Satisfied, she leaves Earth and goes back to Mars.
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
